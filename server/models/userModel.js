44a08732929c45799484d4b1a0174bf032b8e7df
nicoleiii <nagitookomaeda100@gmail.com>
2025-04-02 13:41:58 -0400

    added encryption

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -29,6 +30,19 @@ const userSchema = new mongoose.Schema({
 }, {
     timestamps: true
 });
+
+// Hash the password before saving the user
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Compare entered password with hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
